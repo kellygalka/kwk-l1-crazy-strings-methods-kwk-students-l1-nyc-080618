d195c09aaeef6a9c5ac82236273d2a54aa56576a
kellygalka <kelly3000a1@gmail.com>
2018-08-07 14:38:15 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,6 +1,6 @@
 # write your method here
 
-def crazy_strings(hello,friends)
+def crazy_strings("hello","friends")
 hello = hello.upcase.reverse
 friends = friendz.swapcase
 return "#{hello} #{friends}"
